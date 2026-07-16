9d8f3bdd3cf1c75cbe1369d3d62a2fd5b428e9f1
Ferencz Roudet <ferencz.roudet@co-opev.com>
2026-07-16 13:46:26 +0200

    added 'about' page

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,12 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  NavLink
 } from "react-router";
+
+// UI
+import { Button } from "./components/ui/button";
+import { MessageCircleQuestionMark } from "lucide-react";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -36,6 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <div className="h-screen flex flex-col">
           <nav className="bg-slate-600 text-white p-4 space-x-2">
             { /* liens de navigation */ }
+          <Button /*asChild*/ variant="link" className="text-secondary"><NavLink to="/about"><MessageCircleQuestionMark className="mr-1 h-4 w-4" /> About this project</NavLink></Button>
           </nav>
           <main className="flex-1 flex flex-row overflow-y-scroll">
             {children}
---
+++ app/routes/about.tsx
@@ -0,0 +1,38 @@
+
+
+export default function about(){
+    return(
+        <div className="flex flex-col flex-1 items-start justify-center pl-100"> {/*justify-center */}
+            <h1 className="text-4xl pb-5">About this project</h1>
+
+            <section className="pl-0 pb-5">
+                <h2 className="text-2xl">Why did I create this website ?</h2>
+                <p className=" border-3 border-green-500">I'm a computer science student, who also happens to be a big fan of "The Witcher".<br/> 
+                    I created this website in order to prepare for a university class where I wanted to have a good grade and to have a personal project related to "The Witcher"
+                </p>
+            </section>
+            
+            <section className="pl-0 pb-5">
+                <h2 className="text-2xl">What technologies did I use and why?</h2>
+                <p className=" border-3 border-green-500">
+                    I used the React Router framework combined with Prisma ORM, because those were the technologies used in the class I wanted to prepare for.
+                </p>
+            </section>
+
+            <section className="pl-0 pb-5">
+                <h2 className="text-2xl">Why is the UI bad?</h2>
+                <p className=" border-3 border-green-500">
+                    *RÉPONDRE ICI*
+                </p>
+            </section>
+
+            <section className="pl-0 pb-5">
+                <h2 className="text-2xl">Did I use AI to create this project?</h2>
+                <p className=" border-3 border-green-500">
+                    I used as little chatbots as possible, because I'm against AI for coding. I do not have any AI agent running locally on my PC. I want to think by myself. <br/>
+                    You can find a list of the prompts I did here, mostly to spare time on all the tailwind css aspect (I used tailwind, because we also used it in the uni class I mentionned before).<br/>
+                </p>
+            </section>
+        </div>
+    )
+}
