d54fd595930985fc19c09ee85b6405119a2626b7
Ferencz Roudet <ferencz.roudet@co-opev.com>
2026-07-20 22:34:28 +0200

    wip: comments on a character's page

--- app/routes/characters/character.tsx
+++ app/routes/characters/character.tsx
@@ -4,8 +4,9 @@
 import type { Route } from "../characters/+types/character";
 import { get_character } from "~/models/character.server";
 import { Separator } from "~/components/ui/separator";
-import { Button } from "@base-ui/react/button";
-import { useNavigate } from "react-router";
+import { useNavigate, Form } from "react-router";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 /**
 * Loader
@@ -39,19 +40,27 @@ let navigate = useNavigate();
 export default function character({ loaderData }: Route.ComponentProps){
     const character = loaderData.character
     return(
-        <div>    
-                <div className="pl-20">
+                <div className="flex flex-col flex-1 pl-20">
                     <h1 className="text-3xl pt-4 pb-4">Name : { character.name }</h1>
                     <Separator/>
-                    <h2 className="text-2xl pt-4 pb-4">Profession : { character.profession }</h2>
+                    <h3 className="pt-4 pb-4">Nickname : { character.nickname }</h3>
                     <Separator/>
-                    <h2 className="text-2xl pt-4 pb-4">Race : { character.race }</h2>
+                    <h3 className="pt-4 pb-4">Profession : { character.profession }</h3>
+                    <Separator/>
+                    <h3 className="pt-4 pb-4">Race : { character.race }</h3>
                     <Separator/>                    
-                    <h2 className="text-2xl pt-4 pb-4">Gender : { character.gender }</h2>
+                    <h3 className="pt-4 pb-4">Gender : { character.gender }</h3>
                     <Separator/>     
-                    <h2 className="text-2xl pt-4 pb-4">Description : { character.description }</h2>
-                    <Separator/>                                   
+                    <h3 className="pt-4 pb-4">Description : { character.description }</h3>
+                    <Separator/>
+                    <Form className="pt-20 pb-20" method="post">
+                        <h2 className="text-2xl">Write a comment</h2>
+                        <Input placeholder="title" name="title"></Input>
+                        <Input placeholder="comment" name="text"></Input>
+                        <Button type="submit" name="intent">Submit</Button>
+                    </Form>     
+                    <Separator/>
+                    <h2 className="text-2xl">Comments</h2>
                 </div> 
-        </div>
     )
 }
