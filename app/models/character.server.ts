c653344c73d8816a7f5f04acc5652a9c45a61899
Ferencz Roudet <ferencz.roudet@co-opev.com>
2026-07-23 15:56:41 +0200

    added basic sorting for characters list (alphabetical, reversed alphabetical)

--- app/models/character.server.ts
+++ app/models/character.server.ts
@@ -12,6 +12,22 @@ export async function get_characters_asc(){
     })
 }
 
+/**
+* Get characters in reversed alphabetical order
+***********************************************/
+
+export async function get_characters_desc(){
+    return await prisma.character.findMany({
+        orderBy: {
+            name: "desc"
+        }
+    })
+}
+
+/**
+* Get one character
+*************************************/
+
 export async function get_character(characterId : number){
     return await prisma.character.findUnique({
         where: {
--- app/routes/characters/characters.tsx
+++ app/routes/characters/characters.tsx
@@ -4,11 +4,13 @@
 import { Outlet } from "react-router"
 
 import type { Route } from "./+types/characters"
-import { get_characters_asc } from "~/models/character.server"
+import { get_characters_asc, get_characters_desc } from "~/models/character.server"
 
 import { Button } from "~/components/ui/button"
 import { NavLinkWithQuery } from "~/components/linkWithQuery"
 import { Separator } from "~/components/ui/separator"
+import { ArrowDownAZ, ArrowUpZA } from "lucide-react"
+import { Form } from "react-router"
 
 /**
 * Loader
@@ -19,25 +21,63 @@ export async function loader(){
 }
 
 /**
+* Action
+************************************/
+
+export async function action({ request }: Route.ActionArgs){
+    const formData = await request.formData()
+    const { intent } = Object.fromEntries(formData)
+
+    if(intent === "get_characters_asc"){
+        try{
+            return { characters: await get_characters_asc() }
+        } catch (error){
+            if (error instanceof Error) { // si on a bien capturé une erreur
+                return { error: error.message }; // on retourne (et pas throw) une réponse avec le message d'erreur
+            } else {
+                throw error; // sinon on laisse remonter l'erreur
+            }
+        }
+    }
+
+    if(intent === "get_characters_desc"){
+        try{
+            return { characters: await get_characters_desc() }
+        } catch (error){
+            if (error instanceof Error) { // si on a bien capturé une erreur
+                return { error: error.message }; // on retourne (et pas throw) une réponse avec le message d'erreur
+            } else {
+                throw error; // sinon on laisse remonter l'erreur
+            }
+        }
+    }
+}
+
+/**
 * Template
 ************************************/
 
-export default function characters({ loaderData } : Route.ComponentProps){
-    const { characters } = loaderData
+export default function characters({ loaderData, actionData } : Route.ComponentProps){
+    const { characters } = actionData ? actionData : loaderData
     return(
         <>
             <div className="w-1/4 border-r-2 p-2 space-y-2 max-h-full overflow-y-auto">
-                <h1 className="text-2xl">Characters</h1>
+                <div className="flex flex-row">
+                    <h1 className="text-2xl flex-1">Characters</h1>
+                    <Form method="post"><Button type="submit" name="intent" value="get_characters_asc"><ArrowDownAZ/></Button></Form>
+                    <Form method="post"><Button type="submit" name="intent" value="get_characters_desc"><ArrowUpZA/></Button></Form>   
+                </div>
                 <Separator/>
                 {
                     <ul>
                     {
-                    characters.map((character) => ( 
+                    characters ?characters.map((character) => ( 
                         <Button /*asChild*/ variant="link" size="sm" className="w-full justify-start hover:bg-slate-200">
                             <NavLinkWithQuery children={character.name} to={"/characters/"+character.id}></NavLinkWithQuery>
                         </Button>
 
                     ))
+                    : <p>No characters to display</p>
                     }
                     </ul>
                 }
