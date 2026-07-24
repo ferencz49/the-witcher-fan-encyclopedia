78d1e7569fda6dc53f044258750a291b4b1c59a7
Ferencz Roudet <ferencz.roudet@co-opev.com>
2026-07-24 23:12:46 +0200

    warning message on home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,6 +8,7 @@
 
 import type { Route } from "./+types/home";
 import logo from '../assets/witcher-logo.png'
+import { TriangleAlert } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -22,11 +23,16 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
+    // <div className="flex flex-col flex-1 items-center justify-center">
+    //   <h1 className="text-4xl">Welcome on this fan-made The Witcher encyclopedia</h1>
+    //   <h2 className="text-2xl">This website is a personal project made by Ferencz Roudet Abraham</h2>
+    //   <h3 className="">For more informations about this project click on the link in the footer of the website</h3>
+    //   <img src={logo} className="w-40"></img>    
+    // </div>
+
     <div className="flex flex-col flex-1 items-center justify-center">
-      <h1 className="text-4xl">Welcome on this fan-made The Witcher encyclopedia</h1>
-      <h2 className="text-2xl">This website is a personal project made by Ferencz Roudet Abraham</h2>
-      <h3 className="">For more informations about this project click on the link in the footer of the website</h3>
-      <img src={logo} className="w-40"></img>    
+      <h1 className="flex flex-row text-4xl">Work in progress<TriangleAlert/></h1>
+      <h1 className="text-2xl">This website just go deployed, the development is in a vert erly stage.</h1>
     </div>
   );
 }
